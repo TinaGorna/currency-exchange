61e0074d3b76e919b9f592c13d218d8e598e6b21
TinaGorna <kristina.gorbunova1997@gmail.com>
2021-05-13 21:32:54 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Currency from "./currency/Currency";
+import {createStore} from "redux";
+import {reducers} from "./redux/state";
+import {Provider} from "react-redux";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const store = createStore(reducers);
+    return (
+        <Provider store={store}>
+            <div className="container">
+                <Currency/>
+            </div>
+        </Provider>
+    );
 }
 
 export default App;
---
+++ src/currency/Currency.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CurrencyExchangeContainer from "../container/CurrencyEContainer"
+
+
+const Currency: React.FunctionComponent = () => {
+    return (
+        <React.Fragment>
+            <CurrencyExchangeContainer/>
+        </React.Fragment>
+    );
+};
+
+export default Currency;
+
